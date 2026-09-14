b3a9a6f4b008997e0ab26791b5e1d4012a889626
Yashgamer-x <yashdesai121212@gmail.com>
2026-09-14 02:00:23 -0400

    Refactor `RingAlgorithm` to enhance `preComputeRoot` logic, introduce `parentToChildRequiredRadius`, and improve radius computation

--- src/main/java/com/yashgamerx/ringbasedalgorithm/algorithm/RingAlgorithm.java
+++ src/main/java/com/yashgamerx/ringbasedalgorithm/algorithm/RingAlgorithm.java
@@ -9,12 +9,21 @@ public class RingAlgorithm {
     }
 
     private void preComputeRoot(RingTreeNode rootNode) {
+        // Preprocess all the children first
         rootNode.getChildren().forEach(this::preCompute);
+
+        // Get children count, maximum Radius child node, and the step angle to calculate the safe distance
+        // required from the parent to the child to avoid collision
         var childrenCount = rootNode.getChildren().size();
         var maxRadiusNode = rootNode.getChildren().stream()
                 .max(this::compareNodes);
         var stepAngle = step360AngleBasedOnChildren(childrenCount);
 
+        // If the maxRadiusNode is present, compute the parent radius and set it
+        maxRadiusNode.ifPresent(node -> {
+            var parentRadius = parentToChildRequiredRadius(node.getRadius(), stepAngle);
+            rootNode.setRadius(parentRadius);
+        });
     }
 
     private int compareNodes(RingTreeNode ringTreeNode, RingTreeNode ringTreeNode1) {
@@ -38,6 +47,10 @@ public class RingAlgorithm {
 
     private double step360AngleBasedOnChildren(int children) {
         return Math.PI * 2 / children;
+    }
+
+    private double parentToChildRequiredRadius(double r, double theta) {
+        return (r / Math.tan(theta / 2)) + r;
     }
 
 }
